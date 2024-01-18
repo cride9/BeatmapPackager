a84b1bbe59c3156bd7e2cd384e5fb208edf3fd6a
cride <gamingbalint80@gmail.com>
2024-01-18 09:32:39 +0100

    Add project files.

---
+++ Packager.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace BeatmapPackager {
+    partial class MainForm {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( ) {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager( typeof( MainForm ) );
+            osuFolderLabel = new Label( );
+            osuFolderButton = new Button( );
+            outputFolderLabel = new Label( );
+            outputFolderButton = new Button( );
+            outputTextbox = new RichTextBox( );
+            ExecuteButton = new Button( );
+            SuspendLayout( );
+            // 
+            // osuFolderLabel
+            // 
+            osuFolderLabel.AutoSize = true;
+            osuFolderLabel.Location = new Point( 12, 25 );
+            osuFolderLabel.Name = "osuFolderLabel";
+            osuFolderLabel.Size = new Size( 196, 14 );
+            osuFolderLabel.TabIndex = 1;
+            osuFolderLabel.Text = "Choose osu beatmap folder: ";
+            // 
+            // osuFolderButton
+            // 
+            osuFolderButton.FlatStyle = FlatStyle.Flat;
+            osuFolderButton.Location = new Point( 228, 12 );
+            osuFolderButton.Name = "osuFolderButton";
+            osuFolderButton.Size = new Size( 150, 41 );
+            osuFolderButton.TabIndex = 0;
+            osuFolderButton.Text = "Select osu folder..";
+            osuFolderButton.UseVisualStyleBackColor = true;
+            osuFolderButton.Click +=  sourceButtonPress ;
+            // 
+            // outputFolderLabel
+            // 
+            outputFolderLabel.AutoSize = true;
+            outputFolderLabel.Location = new Point( 12, 72 );
+            outputFolderLabel.Name = "outputFolderLabel";
+            outputFolderLabel.Size = new Size( 210, 14 );
+            outputFolderLabel.TabIndex = 3;
+            outputFolderLabel.Text = "Choose a destination folder: ";
+            // 
+            // outputFolderButton
+            // 
+            outputFolderButton.FlatStyle = FlatStyle.Flat;
+            outputFolderButton.Location = new Point( 228, 59 );
+            outputFolderButton.Name = "outputFolderButton";
+            outputFolderButton.Size = new Size( 150, 41 );
+            outputFolderButton.TabIndex = 2;
+            outputFolderButton.Text = "Select output folder..";
+            outputFolderButton.UseVisualStyleBackColor = true;
+            outputFolderButton.Click +=  destinationButtonPress ;
+            // 
+            // outputTextbox
+            // 
+            outputTextbox.BackColor = Color.FromArgb(     30,     30,     30 );
+            outputTextbox.ForeColor = Color.FromArgb(     255,     128,     128 );
+            outputTextbox.Location = new Point( 384, 12 );
+            outputTextbox.Name = "outputTextbox";
+            outputTextbox.ReadOnly = true;
+            outputTextbox.Size = new Size( 277, 135 );
+            outputTextbox.TabIndex = 4;
+            outputTextbox.Text = "";
+            outputTextbox.WordWrap = false;
+            // 
+            // ExecuteButton
+            // 
+            ExecuteButton.FlatStyle = FlatStyle.Flat;
+            ExecuteButton.Location = new Point( 12, 106 );
+            ExecuteButton.Name = "ExecuteButton";
+            ExecuteButton.Size = new Size( 366, 41 );
+            ExecuteButton.TabIndex = 5;
+            ExecuteButton.Text = "Make beatmap pack";
+            ExecuteButton.UseVisualStyleBackColor = true;
+            ExecuteButton.Click +=  executeButtonPress ;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF( 7F, 14F );
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(     25,     25,     25 );
+            ClientSize = new Size( 673, 156 );
+            Controls.Add( ExecuteButton );
+            Controls.Add( outputTextbox );
+            Controls.Add( outputFolderLabel );
+            Controls.Add( outputFolderButton );
+            Controls.Add( osuFolderLabel );
+            Controls.Add( osuFolderButton );
+            Font = new Font( "Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point,   238 );
+            ForeColor = Color.FromArgb(     212,     212,     212 );
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = ( Icon )resources.GetObject( "$this.Icon" );
+            MaximizeBox = false;
+            Name = "MainForm";
+            Text = "Beatmap Packager";
+            TopMost = true;
+            ResumeLayout( false );
+            PerformLayout( );
+        }
+
+        #endregion
+        private Label osuFolderLabel;
+        private Button osuFolderButton;
+        private Label outputFolderLabel;
+        private Button outputFolderButton;
+        private RichTextBox outputTextbox;
+        private Button ExecuteButton;
+    }
+}
---
+++ Packager.cs
@@ -0,0 +1,89 @@
+using System.DirectoryServices.ActiveDirectory;
+using System.IO.Compression;
+
+namespace BeatmapPackager {
+    public partial class MainForm : Form {
+        public MainForm( ) {
+            InitializeComponent( );
+        }
+
+        string folderPath = null;
+        DirectoryInfo dirInfo = null!;
+
+        private void sourceButtonPress( object sender, EventArgs e ) {
+
+            using ( var fbd = new FolderBrowserDialog( ) ) {
+
+                if ( fbd.ShowDialog( ) == DialogResult.OK && !string.IsNullOrWhiteSpace( fbd.SelectedPath ) ) {
+                    folderPath = fbd.SelectedPath;
+                    osuFolderLabel.Text = folderPath.Split( @"\" ).Last( );
+                }
+            }
+        }
+
+        private void destinationButtonPress( object sender, EventArgs e ) {
+
+            using ( var fbd = new FolderBrowserDialog( ) ) {
+
+                if ( fbd.ShowDialog( ) == DialogResult.OK && !string.IsNullOrWhiteSpace( fbd.SelectedPath ) ) {
+
+                    dirInfo = new( fbd.SelectedPath );
+                    outputFolderLabel.Text = fbd.SelectedPath.Split(@"\").Last();
+                }
+            }
+        }
+
+        private void executeButtonPress( object sender, EventArgs e ) {
+
+            try {
+
+                string[ ] dirs = Directory.GetDirectories( folderPath );
+
+                if ( dirs.Length <= 0 )
+                    throw new Exception( "Chosen beatmap directy does not contain any maps" );
+
+                if ( !dirInfo.Exists ) {
+
+                    DialogResult result = MessageBox.Show( "Would you like to create the output folder?", "Output folder not found", MessageBoxButtons.YesNo );
+                    if ( result == DialogResult.Yes )
+                        dirInfo.Create( );
+                    else
+                        throw new Exception( "Chosen output folder not found or not created yet!" );
+
+                }
+
+                foreach ( var dir in dirs ) {
+
+                    string folderName = dir.Split( @"\" ).Last( );
+                    string outputName = $@"{dirInfo.FullName}\{folderName}.osz";
+                    string sourceFolder = $@"{folderPath}\{folderName}";
+
+                    if ( !File.Exists( outputName ) ) {
+                        ZipFile.CreateFromDirectory( sourceFolder, outputName );
+                        PrintMessage( $"Created: {outputName}\n" );
+                    }
+                    else 
+                        PrintMessage( $"Failed to create: {folderName}.osz\n", true );
+
+                }
+
+            } catch ( Exception exception ) {
+
+                MessageBox.Show( exception.Message );
+                return;
+            }
+        }
+
+        private void PrintMessage( string message, bool error = false ) {
+
+            if ( !error ) {
+                outputTextbox.ForeColor = Color.FromArgb( 255, 0, 192, 0 );
+                outputTextbox.Text += message;
+                return;
+            }
+
+            outputTextbox.ForeColor = Color.FromArgb( 255, 255, 128, 128 );
+            outputTextbox.Text += message;
+        }
+    }
+}
