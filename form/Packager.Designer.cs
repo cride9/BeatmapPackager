50735056b57e9562597f905b5fb3bf91ab558471
MrMazanius <129594988+MrMazanius@users.noreply.github.com>
2025-01-25 22:22:09 +0100

    sss

--- form/Packager.Designer.cs
+++ form/Packager.Designer.cs
@@ -22,133 +22,189 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent( ) {
-            components = new System.ComponentModel.Container( );
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager( typeof( MainForm ) );
-            osuFolderLabel = new Label( );
-            osuFolderButton = new Button( );
-            ExecuteButton = new Button( );
-            cbType = new ComboBox( );
-            comboTooltip = new ToolTip( components );
-            downloadButton = new Button( );
-            progressBar = new ProgressBar( );
-            label1 = new Label( );
-            ProgressUpdate = new System.Windows.Forms.Timer( components );
-            SuspendLayout( );
-            // 
-            // osuFolderLabel
-            // 
-            osuFolderLabel.Location = new Point( 12, 25 );
-            osuFolderLabel.Name = "osuFolderLabel";
-            osuFolderLabel.Size = new Size( 210, 14 );
-            osuFolderLabel.TabIndex = 1;
-            osuFolderLabel.Text = "Choose osu beatmap folder: ";
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            osuFolderButton = new Button();
+            ExecuteButton = new Button();
+            cbType = new ComboBox();
+            comboTooltip = new ToolTip(components);
+            downloadButton = new Button();
+            progressBar = new ProgressBar();
+            ProgressUpdate = new System.Windows.Forms.Timer(components);
+            osuTitle = new Label();
+            osuScriptLabel = new Label();
+            osuFolderLabel = new Label();
+            osuDownloadedItems = new TextBox();
+            osuSelectedScriptLabel = new Label();
+            SuspendLayout();
             // 
             // osuFolderButton
             // 
+            osuFolderButton.BackColor = Color.FromArgb(35, 35, 35);
+            osuFolderButton.FlatAppearance.BorderSize = 0;
             osuFolderButton.FlatStyle = FlatStyle.Flat;
-            osuFolderButton.Location = new Point( 228, 12 );
+            osuFolderButton.Font = new Font("Arial", 10F);
+            osuFolderButton.Location = new Point(131, 80);
             osuFolderButton.Name = "osuFolderButton";
-            osuFolderButton.Size = new Size( 155, 41 );
+            osuFolderButton.Size = new Size(141, 27);
             osuFolderButton.TabIndex = 0;
             osuFolderButton.Text = "Select songs folder";
-            osuFolderButton.UseVisualStyleBackColor = true;
-            osuFolderButton.Click +=  sourceButtonPress ;
+            osuFolderButton.UseVisualStyleBackColor = false;
+            osuFolderButton.Click += sourceButtonPress;
             // 
             // ExecuteButton
             // 
+            ExecuteButton.BackColor = Color.FromArgb(35, 35, 35);
+            ExecuteButton.FlatAppearance.BorderSize = 0;
             ExecuteButton.FlatStyle = FlatStyle.Flat;
-            ExecuteButton.Location = new Point( 12, 42 );
+            ExecuteButton.Font = new Font("Arial", 10F);
+            ExecuteButton.Location = new Point(12, 79);
             ExecuteButton.Name = "ExecuteButton";
-            ExecuteButton.Size = new Size( 104, 39 );
+            ExecuteButton.Size = new Size(113, 27);
             ExecuteButton.TabIndex = 5;
             ExecuteButton.Text = "Create script";
-            ExecuteButton.UseVisualStyleBackColor = true;
-            ExecuteButton.Click +=  executeButtonPress ;
+            ExecuteButton.UseVisualStyleBackColor = false;
+            ExecuteButton.Click += executeButtonPress;
             // 
             // cbType
             // 
-            cbType.BackColor = Color.FromArgb(     30,     30,     30 );
+            cbType.BackColor = Color.FromArgb(30, 30, 30);
             cbType.DropDownStyle = ComboBoxStyle.DropDownList;
             cbType.FlatStyle = FlatStyle.Flat;
-            cbType.ForeColor = Color.FromArgb(     212,     212,     212 );
+            cbType.Font = new Font("Arial", 10F);
+            cbType.ForeColor = Color.White;
             cbType.FormattingEnabled = true;
-            cbType.Items.AddRange( new object[ ] { "PowerShell script", "Packager script" } );
-            cbType.Location = new Point( 209, 59 );
+            cbType.Items.AddRange(new object[] { "PowerShell script", "Packager script" });
+            cbType.Location = new Point(365, 83);
             cbType.Name = "cbType";
-            cbType.Size = new Size( 174, 22 );
+            cbType.Size = new Size(157, 24);
             cbType.TabIndex = 6;
             // 
             // downloadButton
             // 
+            downloadButton.BackColor = Color.FromArgb(35, 35, 35);
+            downloadButton.FlatAppearance.BorderSize = 0;
             downloadButton.FlatStyle = FlatStyle.Flat;
-            downloadButton.Location = new Point( 122, 42 );
+            downloadButton.Font = new Font("Arial", 10F);
+            downloadButton.Location = new Point(278, 80);
             downloadButton.Name = "downloadButton";
-            downloadButton.Size = new Size( 81, 39 );
+            downloadButton.Size = new Size(81, 27);
             downloadButton.TabIndex = 7;
             downloadButton.Text = "Import from pack";
-            downloadButton.UseVisualStyleBackColor = true;
-            downloadButton.Click +=  OnImport ;
+            downloadButton.UseVisualStyleBackColor = false;
+            downloadButton.Click += OnImport;
             // 
             // progressBar
             // 
-            progressBar.BackColor = Color.FromArgb(     25,     25,     25 );
-            progressBar.Location = new Point( 12, 101 );
+            progressBar.BackColor = Color.FromArgb(25, 25, 25);
+            progressBar.Location = new Point(12, 219);
             progressBar.Name = "progressBar";
-            progressBar.Size = new Size( 371, 23 );
+            progressBar.Size = new Size(510, 10);
             progressBar.Step = 1;
             progressBar.TabIndex = 8;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point( 12, 84 );
-            label1.Name = "label1";
-            label1.Size = new Size( 63, 14 );
-            label1.TabIndex = 9;
-            label1.Text = "Progress";
+            progressBar.Click += progressBar_Click;
             // 
             // ProgressUpdate
             // 
             ProgressUpdate.Enabled = true;
-            ProgressUpdate.Tick +=  ProgressBarTick ;
+            ProgressUpdate.Tick += ProgressBarTick;
+            // 
+            // osuTitle
+            // 
+            osuTitle.AutoSize = true;
+            osuTitle.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            osuTitle.Location = new Point(9, 12);
+            osuTitle.Name = "osuTitle";
+            osuTitle.Size = new Size(153, 19);
+            osuTitle.TabIndex = 9;
+            osuTitle.Text = "Beatmap Packager";
+            // 
+            // osuScriptLabel
+            // 
+            osuScriptLabel.AutoSize = true;
+            osuScriptLabel.Location = new Point(102, 58);
+            osuScriptLabel.Name = "osuScriptLabel";
+            osuScriptLabel.Size = new Size(154, 14);
+            osuScriptLabel.TabIndex = 11;
+            osuScriptLabel.Text = "                     ";
+            // 
+            // osuFolderLabel
+            // 
+            osuFolderLabel.AutoSize = true;
+            osuFolderLabel.Font = new Font("Arial", 9F);
+            osuFolderLabel.Location = new Point(12, 40);
+            osuFolderLabel.Name = "osuFolderLabel";
+            osuFolderLabel.Size = new Size(87, 15);
+            osuFolderLabel.TabIndex = 12;
+            osuFolderLabel.Text = "Songs Folder: ";
+            // 
+            // osuDownloadedItems
+            // 
+            osuDownloadedItems.BackColor = Color.FromArgb(35, 35, 35);
+            osuDownloadedItems.Font = new Font("Consolas", 8F);
+            osuDownloadedItems.ForeColor = Color.White;
+            osuDownloadedItems.Location = new Point(12, 112);
+            osuDownloadedItems.Multiline = true;
+            osuDownloadedItems.Name = "osuDownloadedItems";
+            osuDownloadedItems.Size = new Size(510, 101);
+            osuDownloadedItems.TabIndex = 13;
+            osuDownloadedItems.Text = "Item 1\r\nItem 2\r\nItem 3\r\nHello";
+            // 
+            // osuSelectedScriptLabel
+            // 
+            osuSelectedScriptLabel.AutoSize = true;
+            osuSelectedScriptLabel.Font = new Font("Arial", 9F);
+            osuSelectedScriptLabel.Location = new Point(12, 58);
+            osuSelectedScriptLabel.Name = "osuSelectedScriptLabel";
+            osuSelectedScriptLabel.Size = new Size(95, 15);
+            osuSelectedScriptLabel.TabIndex = 14;
+            osuSelectedScriptLabel.Text = "Selected Script: ";
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF( 7F, 14F );
+            AutoScaleDimensions = new SizeF(7F, 14F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(     25,     25,     25 );
-            ClientSize = new Size( 395, 136 );
-            Controls.Add( label1 );
-            Controls.Add( progressBar );
-            Controls.Add( downloadButton );
-            Controls.Add( cbType );
-            Controls.Add( ExecuteButton );
-            Controls.Add( osuFolderLabel );
-            Controls.Add( osuFolderButton );
-            Font = new Font( "Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point,   238 );
-            ForeColor = Color.FromArgb(     212,     212,     212 );
-            FormBorderStyle = FormBorderStyle.FixedSingle;
-            Icon = ( Icon )resources.GetObject( "$this.Icon" );
+            BackColor = Color.FromArgb(25, 25, 25);
+            ClientSize = new Size(534, 241);
+            Controls.Add(osuSelectedScriptLabel);
+            Controls.Add(osuDownloadedItems);
+            Controls.Add(osuFolderLabel);
+            Controls.Add(osuScriptLabel);
+            Controls.Add(osuTitle);
+            Controls.Add(progressBar);
+            Controls.Add(downloadButton);
+            Controls.Add(cbType);
+            Controls.Add(ExecuteButton);
+            Controls.Add(osuFolderButton);
+            Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            ForeColor = Color.FromArgb(212, 212, 212);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Beatmap Packager";
             TopMost = true;
-            Load +=  OnLoad ;
-            ResumeLayout( false );
-            PerformLayout( );
+            Load += OnLoad;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-        private Label osuFolderLabel;
         private Button osuFolderButton;
         private Button ExecuteButton;
         private ComboBox cbType;
         private ToolTip comboTooltip;
         private Button downloadButton;
         private ProgressBar progressBar;
-        private Label label1;
         private System.Windows.Forms.Timer ProgressUpdate;
+        private Label osuTitle;
+        private Label osuFolderLabel;
+        private Label osuScriptLabel;
+        private Label osuFolderLabel;
+        private TextBox osuDownloadedItems;
+        private Label osuSelectedScriptLabel;
     }
 }
